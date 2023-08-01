83e0ad1de34fdc3ca3fafd45a5465aa13b593833
porpol <tonywf15@gmail.com>
2023-08-01 16:07:37 -0400

    added song analyzer template for interaction with Cyanite API as well as a song model template

--- backend/SongSenseiBackend/SongSenseiApp/models.py
+++ backend/SongSenseiBackend/SongSenseiApp/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Text(models.Model):
 	text = models.CharField(max_length = 500, blank = False)
 	time_sent = models.DateTimeField(auto_now_add = True) 
+	
+class Song(models.Model):
+	# put the type of data that would be sent from the frontend for a song
