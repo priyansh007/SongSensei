30b5d52a3a90124a3eb8713b746bdbb5ca452595
kaiyuanli7 <kaiyuanli07@gmail.com>
2023-08-02 14:09:45 -0700

    updated navbar v.1

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,20 +1,23 @@
 import { Link } from "wouter";
+import { useState } from "react";
 
 function Navbar() {
     return (
         <nav className="container mx-auto max-w-3xl text-blue-600 mt-10 px-5">
-            <Link href="/" className="inline-block mr-5 hover:text-blue-400">
-                Home
-            </Link>
-            <Link href="/about" className="inline-block mr-5 hover:text-blue-400">
-                About
-            </Link>  
-            <Link href="/team" className="inline-block mr-5 hover:text-blue-400">
-                Team
-            </Link>  
-            <Link href="/search" className="inline-block mr-5 hover:text-blue-400">
-                Search
-            </Link>  
+            <div style={{ float: "right", fontFamily: "Arial, sans-serif", fontSize: "18px" }}>
+                <Link href="/" className="inline-block mr-5 hover:text-blue-400">
+                    Home
+                </Link>
+                <Link href="/about" className="inline-block mr-5 hover:text-blue-400">
+                    About
+                </Link>  
+                <Link href="/team" className="inline-block mr-5 hover:text-blue-400">
+                    Team
+                </Link>  
+                <Link href="/search" className="inline-block mr-5 hover:text-blue-400">
+                    Search
+                </Link> 
+            </div> 
         </nav>
     );
 }
