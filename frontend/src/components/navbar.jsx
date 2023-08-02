7a669de377849d5d1f13126bfff0721af1d83371
kaiyuanli7 <kaiyuanli07@gmail.com>
2023-08-02 15:03:10 -0700

    Chatgpt revisions to the Navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "wouter";
-import { css } from "@emotion/css";
+import { css, keyframes } from "@emotion/css";
 
 const styles = {
   header: css`
@@ -9,24 +9,25 @@ const styles = {
     justify-content: space-between;
     padding: 10px;
     margin-bottom: 10px; /* Add margin-bottom to create a gap between header and content */
-    background-color: black; /* Set the background color to pure black */
+    background: linear-gradient(135deg, #0070f3, #00b4d8); /* Gradient background color */
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1); /* Add a box shadow for depth */
     color: #fff; /* Set the text color to white */
-    font-family: "Montserrat", sans-serif; /* Use the "Montserrat" font */
+    font-family: Arial, sans-serif; /* Use a fallback font stack */
     font-size: 32px; /* Increase font size for the header */
-    font-weight: 400; /* Use font weight 400 for a thinner and more professional look */
+    font-weight: 600; /* Use font weight 600 for a bold look */
   `,
   pageTitle: css`
     flex: 1; /* Grow to fill remaining space */
     margin-right: 10px; /* Adjust the margin to create a gap */
-    font-family: "Montserrat", sans-serif; /* Use the "Montserrat" font */
-    font-size: 28px; /* Increase font size for the page title */
-    font-weight: 400; /* Use font weight 400 for a thinner and more professional look */
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif; /* Use a modern sans-serif font */
+    font-size: 40px; /* Increase font size for the page title */
+    font-weight: 700; /* Use font weight 700 for a bolder look */
   `,
   navbar: css`
-    font-family: "Montserrat", sans-serif; /* Use the "Montserrat" font */
-    font-size: 24px; /* Increase font size for the navbar */
+    font-family: Arial, sans-serif; /* Use a fallback font stack */
+    font-size: 20px; /* Decrease font size for the navbar */
     color: white;
-    font-weight: 400; /* Use font weight 400 for a thinner and more professional look */
+    font-weight: 600; /* Use font weight 600 for a bold look */
     & > a {
       margin-right: 30px; /* Increase the margin to space out the links more */
       color: #fff;
@@ -34,15 +35,15 @@ const styles = {
       background-color: transparent; /* Set a transparent background color */
       padding: 8px 16px;
       border-radius: 4px;
+      transition: background-color 0.3s ease; /* Add a smooth transition on hover */
       &:hover {
         color: white;
-        background-color: #0070f3;
+        background-color: #003049; /* Darker color when hovered */
       }
     }
   `,
   activeLink: css`
-    background-color: #0070f3;
-    color: white;
+    background-color: #003049; /* Darker color for active link */
   `,
 };
 
