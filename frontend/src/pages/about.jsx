bfaa655a0fafa45de50ec75cff2ff7c245366fa3
sdjfoais <sdjfoais@gmail.com>
2023-08-03 10:50:49 -0700

    Start on About Page

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -1,8 +1,15 @@
 function About() {
     return (
-        <h1 className="text-center text-xl font-bold text-slate-600 mt-20">
-						About Page
-				</h1>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <div className="text-center max-w-3xl">
+                <h1 className="text-7xl font-bold text-black mt-1 mb-8">
+                    Find New Music That Fits Your Taste
+                </h1>
+                <h2 className="text-3xl font-bold text-gray-700 mt-32">
+                    SongSensei is a student-developed program meant to help others find new music similar to songs they already love
+                </h2>
+            </div>
+        </div>
     );
 }
 export default About;
