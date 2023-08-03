94db03d9fe0bd7c375f34417cbb78e9163d7266e
porpol <tonywf15@gmail.com>
2023-08-03 17:40:48 -0400

    remove api key from demo.py

--- backend/CynaiteDemo/demo.py
+++ backend/CynaiteDemo/demo.py
@@ -1,6 +1,6 @@
 import requests
 
-CYANITE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiSW50ZWdyYXRpb25BY2Nlc3NUb2tlbiIsInZlcnNpb24iOiIxLjAiLCJpbnRlZ3JhdGlvbklkIjo2NDcsInVzZXJJZCI6NTMxODAsImFjY2Vzc1Rva2VuU2VjcmV0IjoiMWFlNWJmYjY0M2M5MmMzMzE3MGQ3OGQ2ZGU5ODZjZGY0YjdhNDM3OTBhMGFkMTExOTUyODc1YTQ1OGQ0YzFjMSIsImlhdCI6MTY5MTA4ODU4MH0.vsThJX3YReRKQ_xttft309YQy_xQGLoyanq4uzt0okE'
+CYANITE_API_KEY = 'nothing'
 
 def send_graphql_request():
     url = "https://api.cyanite.ai/graphql"
