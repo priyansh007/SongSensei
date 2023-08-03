d056b334e77f23b79f576bb4756789257edd1158
sdjfoais <sdjfoais@gmail.com>
2023-08-03 11:29:31 -0700

    More to About Page

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -1,8 +1,8 @@
 function About() {
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
+        <><div className="flex flex-col items-center justify-center h-screen">
             <div className="text-center max-w-3xl">
-                <h1 className="text-7xl font-bold text-black mt-1 mb-8">
+                <h1 className="text-7xl font-bold text-black mb-8">
                     Find New Music That Fits Your Taste
                 </h1>
                 <h2 className="text-3xl font-bold text-gray-700 mt-32">
@@ -10,6 +10,34 @@ function About() {
                 </h2>
             </div>
         </div>
+        <div class="container mx-auto py-8">
+            <div class="grid grid-cols-3 gap-4 justify-center">
+                <div class="bg-gray-200 p-4 h-64">
+                    <h1 className="text-2xl font-bold text-black mb-8">
+                        How do I give songs for SongSensei to analyze?
+                    </h1>
+                    <h2 className="text-xl font-bold text-gray-700">
+                        Simply click input song and pick any song on Spotify for SongSensei to analyze.
+                    </h2>
+                </div>
+                <div class="bg-gray-200 p-4 h-64">
+                    <h1 className="text-2xl font-bold text-black mb-8">
+                        How does SongSensei work?
+                    </h1>
+                    <h2 className="text-xl font-bold text-gray-700">
+                        SongSensei uses the Cyanite API to help analyze any song the user inputs and finds songs that have similar BPM, lyrics, mood, ect.
+                    </h2>
+                </div>
+                <div class="bg-gray-200 p-4 h-64">
+                    <h1 className="text-2xl font-bold text-black mb-8">
+                        Why use SongSensei?
+                    </h1>
+                    <h2 className="text-xl font-bold text-gray-700">
+                        Traditional music platforms such as Spotify mainly only account for similar artists or genres when recommending new music. SongSensei looks through every aspect of a song to find the perfect match for you. 
+                    </h2>
+                </div>
+            </div>
+        </div></>
     );
 }
 export default About;
