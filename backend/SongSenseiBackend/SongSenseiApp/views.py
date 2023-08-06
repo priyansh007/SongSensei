ea1e87fdbef4793e6857d8565337aca3ece9fe4e
kaiyuanli7 <kaiyuanli07@gmail.com>
2023-08-05 22:11:16 -0700

    WORKING SPOTIFY ID LINK FROM FRONTTOBACK

--- backend/SongSenseiBackend/SongSenseiApp/views.py
+++ backend/SongSenseiBackend/SongSenseiApp/views.py
@@ -22,8 +22,36 @@ from uploadsongfile.createlibrarytrack import *
 from retrievesimilarsongs.librarysimilarsongs import *
 
 #import cyanite
-
+CYANITE_API_URL = "https://app.cyanite.ai/search?source=spotify&sourceId&sourceUserLibraryId"
+CYANITE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiSW50ZWdyYXRpb25BY2Nlc3NUb2tlbiIsInZlcnNpb24iOiIxLjAiLCJpbnRlZ3JhdGlvbklkIjo2NDYsInVzZXJJZCI6NTI5ODgsImFjY2Vzc1Rva2VuU2VjcmV0IjoiODliZjYzMGZjODNjNWE2NDRlN2U1YTBlMmUzZDlmNzJiZTNiYWNhOTc2MjdjM2QxZTE3Y2ZhODUwN2VkOGFhMSIsImlhdCI6MTY5MTE4MTQyOH0.dZJxm4eYLE80YjiaNDaOcn9SmztHudvGsIj-QFBgEqM'
 # says that this function can do handle POST requests
+
+@api_view(["POST"])
+def track_view(request):
+    if request.method == 'POST':
+        try:
+            track_id = request.data.get('trackId')  # 'trackId' is the key used to send the track ID from the frontend
+            print(f"Received track ID from frontend: {track_id}")
+
+            # Make a request to the Cyanite API to get similar songs
+            headers = {
+                "Authorization": f"Bearer {CYANITE_API_KEY}",
+            }
+            params = {
+                "track_id": track_id,
+                "limit": 10,  # You can adjust the number of similar songs you want to retrieve
+            }
+            response = requests.get(CYANITE_API_URL, headers=headers, params=params)
+            response_data = response.json()
+
+            # Return the similar songs as a response to the frontend
+            return Response(response_data)
+        except Exception as e:
+            print(f"Error processing track ID: {str(e)}")
+            return Response({'error': 'Failed to process track ID.'}, status=500)
+
+    return Response({'error': 'Invalid request method.'}, status=405)
+
 @api_view(["POST"])
 def summarize_view(request):
 	
@@ -59,21 +87,6 @@ def webhook_handler(request):
 
 #def GraphQLView(request):
 #	Query.resolve_get_music_recommendations()
-def track_view(request):
-    if request.method == 'POST':
-        try:
-            track_id = request.POST.get('trackId')  # 'trackId' is the key used to send the track ID from the frontend
-            print(f"Received track ID from frontend: {track_id}")
-            # You can now do whatever you want with the track ID, such as saving it to your database or performing other operations.
-            
-            # Return a success response to the frontend
-            return JsonResponse({'message': 'Track ID received successfully.'})
-        except Exception as e:
-            print(f"Error processing track ID: {str(e)}")
-            return JsonResponse({'error': 'Failed to process track ID.'}, status=500)
-    
-    return JsonResponse({'error': 'Invalid request method.'}, status=405)
-
 @csrf_exempt
 def upload_mp3(request):
 	if request.method == 'POST':
